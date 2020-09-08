440f0b57bda3b99fb9ada74704ed8a728d669b27
constantindjonkam <70917662+constantindjonkam@users.noreply.github.com>
2020-09-07 21:53:47 -0400

    Add files via upload

---
+++ build/precache-manifest.54f7ab5312757a615d7c89b32fd5f0bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3cc3d5c384ca31be7b5845f9c6a103",
+    "url": "/index.html"
+  },
+  {
+    "revision": "461951028b60ae97d266",
+    "url": "/static/css/main.fc5847eb.chunk.css"
+  },
+  {
+    "revision": "eaddb6ea9d9b8ff5573d",
+    "url": "/static/js/2.ff1c8c2f.chunk.js"
+  },
+  {
+    "revision": "3ceb8c17f560e6682fd3523301e674b3",
+    "url": "/static/js/2.ff1c8c2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "461951028b60ae97d266",
+    "url": "/static/js/main.5a845c17.chunk.js"
+  },
+  {
+    "revision": "f5ed15b071ecd5efa918",
+    "url": "/static/js/runtime-main.db20ac27.js"
+  },
+  {
+    "revision": "138b3d891fe4ce71a231503d77ee251a",
+    "url": "/static/media/notfound.138b3d89.png"
+  }
+]);
